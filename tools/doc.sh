83ce21eb73f9c31bf046a9ec57075c551967b90d
Ahmet <aemrebas.dev@gmail.com>
2024-10-15 09:12:15 -0500

    doc scripts

--- tools/doc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-cd ../libs;
-cd type;
-compodoc --tsconfig tsconfig.json --output ../../documentation/type;
-
--- tools/doc.ts
+++ tools/doc.ts
@@ -3,10 +3,17 @@
 import { execSync } from 'child_process';
 import { readdirSync } from 'fs';
 import { join } from 'path';
+import { chdir } from 'process';
 
 const libs = readdirSync(join(__dirname, '..', 'libs'));
 
-for (const lib of libs) {
-  execSync(`npx @compodoc/compodoc libs/${lib} -s=false -p=libs/${lib}/tsconfig.json -n=${lib} -d=documentation/${lib}`);
+function moveInLib(lib: string) {
+  chdir(join(__dirname, '..', 'libs', lib));
 }
-// compodoc --tsconfig tsconfig.json --output ../../documentation/type
+
+for (const lib of libs) {
+  moveInLib(lib);
+  execSync(
+    `compodoc --tsconfig tsconfig.json --output ../../documentation/${lib};`
+  );
+}
