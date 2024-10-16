a768fdf6a4295e3311503117394400df9659946d
Ahmet <aemrebas.dev@gmail.com>
2024-10-16 16:08:50 -0500

    property

--- libs/property/src/lib/dto.decorator.ts
+++ libs/property/src/lib/dto.decorator.ts
@@ -1,5 +1,10 @@
 import { Exclude, ExcludeOptions } from 'class-transformer';
 
+/**
+ * Exclude the extra properties form the data transfer object
+ * @param options
+ * @returns
+ */
 export function Dto(options?: ExcludeOptions): ClassDecorator {
   return (t) => {
     Exclude(options)(t);
